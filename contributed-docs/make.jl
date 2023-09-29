60ce323c762ca3e1b3f89eb56973713f8b85fe60
ddahlbom <david.dahlbom@gmail.com>
2023-09-29 12:34:31 -0400

    Auto-build 2023-09-29T12:34:31.050

--- contributed-docs/make.jl
+++ contributed-docs/make.jl
@@ -1,24 +1,25 @@
 using Literate, Git, Dates
 
 root = dirname(@__FILE__)
-contrib_docs_dir = joinpath(root, "src") 
+src_dir = joinpath(root, "src") 
 build_dir = joinpath(root, "build")
 
 # If no arguments given, rebuild all notebooks
 docnames = if length(ARGS) == 0
-    filter(name -> split(name, ".")[end] == "jl", readdir(contrib_docs_dir))
+    filter(name -> split(name, ".")[end] == "jl", readdir(src_dir))
 else
     ARGS
 end
 
 # Build the notebooks
 map(docnames) do docname
-    Literate.markdown(joinpath(contrib_docs_dir, docname), build_dir; execute=true, documenter=false, credit=false)
+    Literate.markdown(joinpath(src_dir, docname), build_dir; execute=true, documenter=false, credit=false)
 end
 
 # Sync with github
-cd("..")
+cd(joinpath(root, ".."))
 run(`$(git()) pull`) # Make sure up to date
+run(git(["add", src_dir*"/*.jl"]))
 run(git(["add", build_dir*"/*.md"]))
 run(git(["add", build_dir*"/*.png"]))
 run(`$(git()) commit -am "Auto-build $(string(Dates.now()))"`)
