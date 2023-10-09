7917fc5167d758f94b02e6e2b920fa77c6268dd1
Sam Quinn <sq@gatech.edu>
2023-10-09 17:54:52 -0400

    Add susceptibility calculator and eigenmode viewer

---
+++ Sam-GT/eigenmodes/eigenmode_viewer.jl
@@ -0,0 +1,385 @@
+# This script is an experiment which visualizes individual spin wave oscillations
+# in real space. Run one of the example_* functions at the bottom to see it in action!
+# 
+# Requires Sunny.jl PR#174, so this won't work on main Sunny as of this writing (Oct 9)
+#
+# The correctness of the visualization is not yet verified--see comments within--and also
+# currently only really shows the dipole sector.
+
+using Sunny, GLMakie, Observables, LinearAlgebra
+
+# Steal these internal functions required for the eigenmode computation
+import Sunny: mk_bogoliubov!, to_reshaped_rlu, swt_hamiltonian_SUN!, natoms
+
+function get_eigenmodes(swt,q; verbose = false)
+    (; sys, data, observables) = swt
+
+    Nm = natoms(sys.crystal) # Number of atoms in magnetic unit cell
+    N = sys.Ns[1] # The N in SU(N), giving the total number of boson flavors
+    Nf = N - 1 # Number of uncondensed boson flavors
+    nmodes = Nf * Nm
+
+    Hmat = zeros(ComplexF64, 2*nmodes, 2*nmodes)
+    Vmat = zeros(ComplexF64, 2*nmodes, 2*nmodes)
+    disp = zeros(Float64, nmodes)
+
+    bogoliubov! = mk_bogoliubov!(nmodes)
+
+    q_reshaped = to_reshaped_rlu(swt.sys, Sunny.Vec3(q))
+
+    if sys.mode == :SUN
+        swt_hamiltonian_SUN!(swt, q_reshaped, Hmat)
+    elseif sys.mode == :dipole
+        error("Dipole mode not supported")
+    end
+    #println("Original Hamiltonian:")
+    H0 = copy(Hmat)
+    #display(H0)
+    #display(eigen(H0))
+
+    bogoliubov!(disp, Vmat, Hmat, swt.energy_tol, true)
+
+    # Now, Vmat contains the information about the eigenmodes as columns.
+    # The way this works is that the first [nmodes] columns describe the
+    # [nmodes] many boson deletion operators, as linear combinations of both
+    # the original boson deletion operators (top half of column) and the original boson
+    # creation operators (bottom half of column).
+    #
+    # [[The second half of the columns contain the boson creation operators in a similar
+    # format, but in reverse order, e.g. it goes [b1,b2,b†2,b†1]. But the creation operators
+    # are not needed because they can be inferred from the deletion operators. The fact
+    # that they can be inferred is equivalent to the dagger operation being preserved
+    # by the bogoliubov transform V]]
+    #
+    # The operators within each half-column come in blocks.
+    # The length of the block corresponds to the boson flavor index, which runs
+    # from 2 to N (where N is as in SU(N)). The #1 is the 'ground state' boson which
+    # was condensed away.
+    #
+    # There is one such (2:N) block for each atom in the magnetic unit cell
+
+
+    bases = swt.data.local_quantization_basis
+
+    eigen_mode_displacements = zeros(ComplexF64,N,Nm,nmodes)
+
+    # Only loops over the boson deletion operators
+    for eigen_mode = 1:nmodes
+      # Describes the operator by its coefficients in the linear
+      # combination ∑ᵢ λᵢaᵢ where aᵢ runs over both the deletion and
+      # creation operators for the *original* non-bogoliubov bosons
+      bogoliubov_deletion_operator = Vmat[:,eigen_mode]
+
+      # Now we construct the (real-space, eventually) displacement
+      # associated with each eigenmode
+      for atom = 1:Nm
+        # Skip the first (atom-1) blocks
+        offset = (atom - 1) * Nf
+
+        # Get the combination of original bosons relevant for this atom
+        deletion_operators_on_this_atom = bogoliubov_deletion_operator[offset .+ (1:Nf)]
+        creation_operators_on_this_atom = bogoliubov_deletion_operator[nmodes .+ offset .+ (1:Nf)]
+
+        # Get the local quantization basis at this atom.
+        # This is actually a basis for the *tangent space* of the space
+        # of coherent states, rooted at the ground state coherent state.
+        # The basis is orthogonal, and there is one boson flavor associated
+        # with each basis vector.
+        basis = bases[:,:,atom]
+
+        # The first basis vector in the basis is along the 'ground state'/longitudinal mode
+        # and it got condensed away. There will be no eigenmode displacements in that
+        # direction.
+        uncondensed_basis = basis[:,2:N]
+
+        # Loop over only the uncondensed bosons
+        for original_uncondensed_boson = 2:N
+          # The bosons all got shifted one to the left when we chopped of the
+          # one which was condensed away.
+          boson_ix = original_uncondensed_boson - 1
+
+          this_displacement = uncondensed_basis[:,boson_ix]
+          this_amplitude = deletion_operators_on_this_atom[boson_ix]
+          eigen_mode_displacements[:,atom,eigen_mode] .+= this_displacement .* this_amplitude
+
+          # This says that the basis vectors corresponding to the creation operators
+          # are the complex conjugates of the basis vectors corresponding to the deletion
+          # operators. Is this true? Who knows...
+          this_conj_displacement = conj.(uncondensed_basis[:,boson_ix])
+          this_conj_amplitude = creation_operators_on_this_atom[boson_ix]
+          eigen_mode_displacements[:,atom,eigen_mode] .+= this_conj_displacement .* this_conj_amplitude
+
+        end
+      end
+    end
+
+
+
+    if verbose
+      println("V matrix:")
+      display(Vmat)
+      println("Diagonalized V'HV:")
+      display(Vmat' * H0 * Vmat)
+      println("Bases")
+      display(bases)
+      println("Eigenmodes (columns are atoms)")
+      for m = 1:nmodes
+        println()
+        println("Mode #$m with energy $(disp[m])")
+        display(eigen_mode_displacements[:,:,m])
+        #=
+        for i = 1:Nm
+          println("Atom#$i:")
+          display(eigen_mode_displacements[:,i,m])
+          println("Δ[dipole] = ")
+          orig_dipole = expected_spin(sys.coherents[i])
+          eps = 1e-5
+          new_dipole = expected_spin(SVector{2}(sys.coherents[i] .+ eps .* eigen_mode_displacements[:,i,m]))
+          #display(orig_dipole)
+          #display(new_dipole)
+          display((new_dipole .- orig_dipole) ./ eps)
+        end
+        =#
+      end
+      println("Eigenenergies")
+      display(disp)
+      println("Nm = $Nm, N = $N, Nf = $Nf, nmodes = $nmodes")
+      display(sys.coherents)
+    end
+    return H0, Vmat, bases, eigen_mode_displacements, disp
+end
+
+function plot_eigenmode(displacements, swt::SpinWaveTheory; kwargs...)
+    fig = Figure()
+    ax = LScene(fig[1, 1]; show_axis = false)
+    plot_eigenmode!(ax, displacements, swt::SpinWaveTheory; kwargs...)
+    fig
+end
+
+function plot_eigenmode!(ax, displacements, swt::SpinWaveTheory; t = nothing, kwargs...)
+  Sunny.Plotting.plot_spins!(ax,swt.sys;kwargs...)
+
+  tweaked = Observable(zeros(Vec3f,size(swt.sys.dipoles)))
+  coherents_scratch = copy(swt.sys.coherents)
+
+  t = isnothing(t) ? Observable(0.) : t
+
+  on(displacements,update = true) do disps
+    notify(t)
+  end
+
+  on(t) do time
+    disps = displacements[]
+    # Requires single-site swt system
+    for i = 1:size(coherents_scratch,4)
+      coherents_scratch[i] = swt.sys.coherents[i] .+ cos(time) .* disps[:,i]
+      tweaked[][i] = Sunny.expected_spin(coherents_scratch[i])
+    end
+    notify(tweaked)
+  end
+
+  # TODO: ghost spins are currently inaccurate, since they should pick up a phase factor
+  Sunny.Plotting.plot_spins!(ax,swt.sys;color = :blue,spin_data = tweaked,kwargs...)
+end
+
+if !(:eigenmode_viewer_screen ∈ names(Main))
+  global eigenmode_viewer_screen = nothing
+end
+function interact_eigenmodes(swt::SpinWaveTheory, qs, formula; kwargs...)
+    # The background band structure plot
+  fig = Figure()
+  ax = Axis(fig[1,1], title = "Click a mode! (Spacebar to animate)", xticklabelsvisible = false, xrectzoom = false, yrectzoom = false)
+  dispersion, intensity = intensities_bands(swt, qs, formula)
+  Sunny.Plotting.plot_band_intensities!(ax, dispersion, intensity)
+
+  # The marker showing the user-selected oscillation
+  marker_points = Observable(Point2f[Point2f(NaN,NaN)])
+  marker_colors = Observable(Float64[NaN])
+  sc_marker = scatter!(ax, marker_points, color = marker_colors, strokewidth = 1.)
+
+  # The eigenmode viewer window
+  global eigenmode_viewer_screen
+  if isnothing(eigenmode_viewer_screen) || eigenmode_viewer_screen.window_open[] == false
+    eigenmode_viewer_screen = GLMakie.Screen()
+  end
+  fig_mode = Figure()
+  ax_mode = LScene(fig_mode[1,1]; show_axis = false)
+  t = Observable(0.) # Animation time
+  rendered_displacements = Observable(zeros(ComplexF64,swt.sys.Ns[1],natoms(swt.sys.crystal)))
+  plot_eigenmode!(ax_mode, rendered_displacements, swt; t)
+
+  # Update the eigenviewer based on the user-selected position
+  function do_update(;mp = nothing)
+    # mouse position = nothing on click, but we already know mp when dragging
+    mp = isnothing(mp) ? events(ax).mouseposition[] : mp
+
+    # Convert from screen-space to data-space
+    bbox = ax.layoutobservables.computedbbox[]
+    c = (mp .- bbox.origin) ./ bbox.widths
+    if 0 < c[1] < 1 && 0 < c[2] < 1
+      data_bbox = ax.finallimits[]
+      data_space_c = data_bbox.origin .+ c .* data_bbox.widths
+      q,ωclick = data_space_c
+
+      # Interpolate between the provided q points
+      q_int = floor(Int64,q)
+      if q_int + 1 > length(qs) || q_int < 1 # if outside q range of plot
+        return
+      end
+      τ = q - q_int
+      q_interp = (1-τ) .* qs[q_int] .+ τ .* qs[q_int+1]
+
+      # Perform the eigenmode analysis
+      _H, _V, _bases, displacements, disp = get_eigenmodes(swt,q_interp)
+
+      # Snap to the nearest band (vertically only)
+      _, ix = findmin(abs.(disp .- ωclick))
+
+      # Move the marker
+      marker_points[][1] = Point2f(q,disp[ix])
+      marker_colors[][1] = 1. # TODO: intensity coloring?
+      notify(marker_points)
+
+      # Update eigenmode viewer with the newly selected mode
+      rendered_displacements[] .= displacements[:,:,ix]
+      notify(rendered_displacements)
+      ax.title[] = "q = [$(join(Sunny.number_to_simple_string.(q_interp,digits=3),","))], ω = $(Sunny.number_to_simple_string(disp[ix],digits=3))"
+    end
+  end
+
+  # Update on left click
+  on(events(fig).mousebutton, priority = 2) do event
+    if event.button == Mouse.left
+      if event.action == Mouse.press
+        do_update()
+        return Consume(true)
+      end
+    end
+  end
+
+  # Update on left click-drag
+  on(async_latest(events(fig).mouseposition), priority = 2) do mp
+    if events(fig).mousebutton[].button == Mouse.left
+      if events(fig).mousebutton[].action == Mouse.press
+        do_update(;mp)
+        return Consume(true)
+      end
+    end
+  end
+
+  # Lock so if we're already animating
+  # we don't do it again over top of itself
+  lck = ReentrantLock()
+
+  # Animate on spacebar
+  on(events(fig_mode).keyboardbutton) do event
+    if event.key == Keyboard.space
+      if event.action == Keyboard.press
+          @async begin
+            if trylock(lck)
+              for t0 in range(0,2π,length = 80)
+                omega = marker_points[][1][2]
+                t[] = omega .* t0
+                sleep(1/30)
+              end
+              unlock(lck)
+            end
+          end
+        return Consume(true)
+      end
+    end
+  end
+  # Forward spacebar to other window if needed
+  connect!(events(fig_mode).keyboardbutton,events(fig).keyboardbutton)
+  
+  # Clear eigenmode screen and display both windows
+  empty!(eigenmode_viewer_screen)
+  display(eigenmode_viewer_screen,fig_mode)
+  display(eigenmode_viewer_screen)
+  fig
+end
+
+function example_eigenmodes()
+  sys = System(Sunny.cubic_crystal(), (1,2,1), [SpinInfo(1;S=1/2,g=1)], :SUN, units = Units.theory)
+  set_external_field!(sys,[0,0,0.5])
+  set_exchange!(sys,-1.,Bond(1,1,[0,1,0]))
+  randomize_spins!(sys)
+  minimize_energy!(sys)
+  minimize_energy!(sys)
+  minimize_energy!(sys)
+
+  swt = SpinWaveTheory(sys)
+  qs = [[0,k,0] for k = range(0,1,length=20)]
+  formula = intensity_formula(swt,:perp, kernel = delta_function_kernel)
+  interact_eigenmodes(swt, qs, formula)
+end
+
+function example_fei2()
+  a = b = 4.05012  # Lattice constants for triangular lattice
+  c = 6.75214      # Spacing in the z-direction
+  latvecs = lattice_vectors(a, b, c, 90, 90, 120) # A 3x3 matrix of lattice vectors that
+                                                  # define the conventional unit cell
+  positions = [[0, 0, 0], [1/3, 2/3, 1/4], [2/3, 1/3, 3/4]]  # Positions of atoms in fractions
+                                                             # of lattice vectors
+  types = ["Fe", "I", "I"]
+  FeI2 = Crystal(latvecs, positions; types)
+  cryst = subcrystal(FeI2, "Fe")
+  sys = System(cryst, (4,4,4), [SpinInfo(1, S=1, g=2)], :SUN, seed=2)
+  J1pm   = -0.236
+  J1pmpm = -0.161
+  J1zpm  = -0.261
+  J2pm   = 0.026
+  J3pm   = 0.166
+  J′0pm  = 0.037
+  J′1pm  = 0.013
+  J′2apm = 0.068
+
+  J1zz   = -0.236
+  J2zz   = 0.113
+  J3zz   = 0.211
+  J′0zz  = -0.036
+  J′1zz  = 0.051
+  J′2azz = 0.073
+
+  J1xx = J1pm + J1pmpm
+  J1yy = J1pm - J1pmpm
+  J1yz = J1zpm
+
+  set_exchange!(sys, [J1xx   0.0    0.0;
+                      0.0    J1yy   J1yz;
+                      0.0    J1yz   J1zz], Bond(1,1,[1,0,0]))
+  set_exchange!(sys, [J2pm   0.0    0.0;
+                      0.0    J2pm   0.0;
+                      0.0    0.0    J2zz], Bond(1,1,[1,2,0]))
+  set_exchange!(sys, [J3pm   0.0    0.0;
+                      0.0    J3pm   0.0;
+                      0.0    0.0    J3zz], Bond(1,1,[2,0,0]))
+  set_exchange!(sys, [J′0pm  0.0    0.0;
+                      0.0    J′0pm  0.0;
+                      0.0    0.0    J′0zz], Bond(1,1,[0,0,1]))
+  set_exchange!(sys, [J′1pm  0.0    0.0;
+                      0.0    J′1pm  0.0;
+                      0.0    0.0    J′1zz], Bond(1,1,[1,0,1]))
+  set_exchange!(sys, [J′2apm 0.0    0.0;
+                      0.0    J′2apm 0.0;
+                      0.0    0.0    J′2azz], Bond(1,1,[1,2,1]))
+
+  D = 2.165
+  S = spin_operators(sys, 1)
+  set_onsite_coupling!(sys, -D*S[3]^2, 1)
+
+  randomize_spins!(sys)
+  minimize_energy!(sys);
+
+  sys_min = reshape_supercell(sys, [1 0 0; 0 1 -2; 0 1 2])
+  randomize_spins!(sys_min)
+  minimize_energy!(sys_min)
+
+  swt = SpinWaveTheory(sys_min)
+
+  q_points = [[0,0,0], [1,0,0], [0,1,0], [1/2,0,0], [0,1,0], [0,0,0]];
+  density = 50
+  path, xticks = reciprocal_space_path(cryst, q_points, density);
+  formula = intensity_formula(swt,:perp, kernel = delta_function_kernel)
+  interact_eigenmodes(swt, path, formula)
+end
---
+++ Sam-GT/susceptibility/susceptibility.jl
@@ -0,0 +1,221 @@
+# This script computes the susceptibility χ using Sunny's LSWT tools
+# Run one of the example_* functions at the bottom to see it in action!
+# 
+# Requires Sunny.jl PR#174, so this might not work on main Sunny as of this writing (Oct 9)
+
+
+# To use the complex plots, you need to install this unregistered package:
+#
+#   Pkg.add(PackageSpec(url="https://github.com/luchr/ComplexPortraits.jl", rev="master"))
+#
+using ComplexPortraits
+using Sunny, GLMakie, StaticArrays
+
+# Steal this internal type
+import Sunny: BandStructure
+
+function plot_susceptibility(band_structure;kwargs...)
+    f = Figure()
+    ax = Axis(f[1,1]; xlabel = "Energy (meV)", ylabel = "χ(iω)")
+    plot_susceptibility!(ax,band_structure;kwargs...)
+    f
+end
+
+function plot_susceptibility!(ax,band_structure::BandStructure{NBands,ComplexF64};part = abs, energies = nothing,decay = 0.1) where NBands
+    energies = if isnothing(energies)
+        peak_bounds = extrema(band_structure.dispersion)
+
+        autocutoff = 1e-1
+        max_peak = maximum(abs.(band_structure.intensity))
+        # A/x = cutoff
+        # x = A/cutoff
+        x_cut = max_peak / autocutoff
+
+        num_steps_per_HWHM = 16
+        range(peak_bounds[1] - x_cut, peak_bounds[2] + x_cut; step = decay / num_steps_per_HWHM)
+    else
+        energies
+    end
+    χ = map(s -> susceptibility_spectral_function(band_structure,s),decay .+ im .* energies)
+    lines!(ax,energies,part.(χ))
+end
+
+
+function correlator_spectral_function(band_structure::BandStructure{NBands,ComplexF64},s) where NBands
+    # Theorem:
+    # ========
+    # If the fourier transform of <B(t) A> has a term
+    #   2π * Aᵢ * δ(ω - ωᵢ)
+    # then the laplace transform has a corresponding term:
+    #   Aᵢ / (s - iωᵢ)
+    val = 0. + 0im
+    for i = 1:NBands
+        val += band_structure.intensity[i] / (s - im * band_structure.dispersion[i])
+    end
+    return val
+end
+
+# Usually, χ(s) is related to the Laplace transform of the correlator <B(t) A>
+# by symmetrizing over [ωC-†] (with no factor 1/2), which is the simultaneous:
+# - replacing ω → -ω
+# - complex conjugation
+# - multiplication by (-1)
+# - replacing A → A†
+#
+# But if we assume A = A†, then we just need [ωC-], which is what this function implements.
+function susceptibility_spectral_function(band_structure::BandStructure{NBands,ComplexF64},s) where NBands
+    val = 0. + 0im
+    for i = 1:NBands
+        val += + band_structure.intensity[i] / (s - im * band_structure.dispersion[i])
+        val += - conj(band_structure.intensity[i] / (s + im * band_structure.dispersion[i]))
+    end
+    return val
+end
+
+
+function intensities_spectral_function(swt::SpinWaveTheory, ks, ωvals, formula; susceptibility = true, decay = 0.1, part = identity)
+    if !isnothing(formula.kernel)
+        error("intensities_correlator_spectral_function: Can't compute spectral function if a broadening kernel is applied.\nTry intensity_formula(...; kernel = delta_function_kernel)")
+    end
+
+    # Get the type parameter from the BandStructure
+    return_type = typeof(formula).parameters[1].parameters[2]
+    if return_type != ComplexF64
+        error("Can't compute spectral function when the intensity isn't complex-valued")
+    end
+
+    spectral_f = susceptibility ? susceptibility_spectral_function : correlator_spectral_function
+
+    is = zeros(typeof(part(0. +1im)), size(ks)..., length(ωvals))
+    for kidx in CartesianIndices(ks)
+        band_structure = formula.calc_intensity(swt, SVector{3,Float64}(ks[kidx]))
+        χ = map(s -> spectral_f(band_structure,s),decay .+ im .* ωvals)
+        is[kidx,:] .= part.(χ)
+    end
+    return is
+end
+
+
+function four_panel_plot(ks,energies,data,name)
+  f = Figure()
+  ax_r = Axis(f[1,2],title = "Real part of $name")
+  ax_i = Axis(f[1,3],title = "Imag part of $name")
+  ax_abs = Axis(f[2,2],title = "|$name|")
+  ax_arg = Axis(f[2,3],title = "arg $name")
+
+  hm = heatmap!(ax_r,ks,energies,real.(data),colormap=:redgreensplit)
+  cr = hm.calculated_colors[].colorrange[]
+  sym_range = maximum(abs.(cr))
+  hm.colorrange[] = [-sym_range,sym_range]
+  Colorbar(f[1,1],hm)
+
+  hm = heatmap!(ax_i,ks,energies,imag.(data))
+  Colorbar(f[1,4],hm)
+
+  hm = heatmap!(ax_abs,ks,energies,abs.(data))
+  Colorbar(f[2,1],hm)
+
+  csp = ComplexPortraits.cs_p()
+  coloring = z -> csp(0.,z)
+  hm = image!(ax_arg,ks,energies,coloring.(data))
+  #Colorbar(f[2,4],hm)
+  f
+end
+
+function example_simple()
+  sys = System(Sunny.cubic_crystal(), (1,2,1), [SpinInfo(1;S=1/2,g=1)], :SUN, units = Units.theory)
+  set_external_field!(sys,[0,0,0.5])
+  set_exchange!(sys,-1.,Bond(1,1,[0,1,0]))
+  randomize_spins!(sys)
+  minimize_energy!(sys)
+  minimize_energy!(sys)
+  minimize_energy!(sys)
+
+  swt = SpinWaveTheory(sys)
+  ks = range(0,1,length=200)
+  qs = [[0,k,0] for k in ks]
+  formula = intensity_formula(swt, [(:Sx,:Sy)], kernel = delta_function_kernel) do k,ω,S
+    S[1]
+  end
+  energies = range(-5,5,length = 200)
+
+  data = intensities_spectral_function(swt, qs, energies, formula, susceptibility = true)
+  four_panel_plot(ks,energies,data,"χxy")
+end
+
+function example_fei2()
+  a = b = 4.05012  # Lattice constants for triangular lattice
+  c = 6.75214      # Spacing in the z-direction
+  latvecs = lattice_vectors(a, b, c, 90, 90, 120) # A 3x3 matrix of lattice vectors that
+                                                  # define the conventional unit cell
+  positions = [[0, 0, 0], [1/3, 2/3, 1/4], [2/3, 1/3, 3/4]]  # Positions of atoms in fractions
+                                                             # of lattice vectors
+  types = ["Fe", "I", "I"]
+  FeI2 = Crystal(latvecs, positions; types)
+  cryst = subcrystal(FeI2, "Fe")
+  sys = System(cryst, (4,4,4), [SpinInfo(1, S=1, g=2)], :SUN, seed=2)
+  J1pm   = -0.236
+  J1pmpm = -0.161
+  J1zpm  = -0.261
+  J2pm   = 0.026
+  J3pm   = 0.166
+  J′0pm  = 0.037
+  J′1pm  = 0.013
+  J′2apm = 0.068
+
+  J1zz   = -0.236
+  J2zz   = 0.113
+  J3zz   = 0.211
+  J′0zz  = -0.036
+  J′1zz  = 0.051
+  J′2azz = 0.073
+
+  J1xx = J1pm + J1pmpm
+  J1yy = J1pm - J1pmpm
+  J1yz = J1zpm
+
+  set_exchange!(sys, [J1xx   0.0    0.0;
+                      0.0    J1yy   J1yz;
+                      0.0    J1yz   J1zz], Bond(1,1,[1,0,0]))
+  set_exchange!(sys, [J2pm   0.0    0.0;
+                      0.0    J2pm   0.0;
+                      0.0    0.0    J2zz], Bond(1,1,[1,2,0]))
+  set_exchange!(sys, [J3pm   0.0    0.0;
+                      0.0    J3pm   0.0;
+                      0.0    0.0    J3zz], Bond(1,1,[2,0,0]))
+  set_exchange!(sys, [J′0pm  0.0    0.0;
+                      0.0    J′0pm  0.0;
+                      0.0    0.0    J′0zz], Bond(1,1,[0,0,1]))
+  set_exchange!(sys, [J′1pm  0.0    0.0;
+                      0.0    J′1pm  0.0;
+                      0.0    0.0    J′1zz], Bond(1,1,[1,0,1]))
+  set_exchange!(sys, [J′2apm 0.0    0.0;
+                      0.0    J′2apm 0.0;
+                      0.0    0.0    J′2azz], Bond(1,1,[1,2,1]))
+
+  D = 2.165
+  S = spin_operators(sys, 1)
+  set_onsite_coupling!(sys, -D*S[3]^2, 1)
+
+  randomize_spins!(sys)
+  minimize_energy!(sys);
+
+  sys_min = reshape_supercell(sys, [1 0 0; 0 1 -2; 0 1 2])
+  randomize_spins!(sys_min)
+  minimize_energy!(sys_min)
+
+  swt = SpinWaveTheory(sys_min)
+
+  q_points = [[0,0,0], [1,0,0], [0,1,0], [1/2,0,0], [0,1,0], [0,0,0]];
+  density = 50
+  path, xticks = reciprocal_space_path(cryst, q_points, density);
+
+  formula = intensity_formula(swt, [(:Sx,:Sy)], kernel = delta_function_kernel) do k,ω,S
+    S[1]
+  end
+  energies = range(-8,8,length = 400)
+
+  data = intensities_spectral_function(swt, path, energies, formula, decay = 0.2, susceptibility = true)
+  four_panel_plot(1:length(path),energies,data,"χxy")
+end
+
