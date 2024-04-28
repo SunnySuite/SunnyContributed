6b4224d393608e82b26c56e3d7545eb24b313270
Sam Quinn <sq@gatech.edu>
2024-04-28 12:57:44 -0400

    Update ll_lswt_limit.jl

--- initiatives/Sam-GT/realspace/ll_lswt_limit.jl
+++ initiatives/Sam-GT/realspace/ll_lswt_limit.jl
@@ -1,134 +1,141 @@
-using Sunny, LinearAlgebra, StaticArrays
+using Sunny, LinearAlgebra, StaticArrays, GLMakie
 
+# Setup system
 cryst = Crystal(I(3),[[0,0,0],[0.5,0.5,0.5]],1)
 sys = System(cryst,(1,1,2),[SpinInfo(1,S=1,g=2),SpinInfo(2,S=1,g=2)],:dipole)
 
 B_z_ext = 2.3
 set_external_field!(sys,[0,0,B_z_ext])
-#set_exchange!(sys,1.,Bond(1,1,[0,0,1]))
+set_exchange!(sys,-1.,Bond(1,1,[0,0,1]))
+set_exchange!(sys,-1.,Bond(1,2,[0,0,0]))
 #set_exchange!(sys,1.,Bond(2,2,[0,0,1]))
 #set_onsite_coupling!(sys,S -> -0.1 * S[3]^2,1)
 #set_onsite_coupling!(sys,S -> -0.1 * S[3]^2,2)
 
 randomize_spins!(sys)
 minimize_energy!(sys)
-println("Dipoles:")
-display(sys.dipoles)
 
-latsize_final = (2,3,4)
-#function finite_spin_wave_theory(sys,latsize_final)
-
-  # Enforce periodic system
-  cellsize_mag = Sunny.cell_shape(sys) * diagm(Sunny.Vec3(sys.latsize))
-  sys_unit = Sunny.reshape_supercell_aux(sys, (1,1,1), cellsize_mag)
-  sys_periodic = repeat_periodically(sys_unit,latsize_final)
-
-  swt = SpinWaveTheory(sys_unit)
-
-
-  isc = instant_correlations(sys_periodic)
-  add_sample!(isc,sys_periodic)
-
-sys_periodic_orig = Sunny.clone_system(sys_periodic)
-function mk_dt()
-  s = Sunny.clone_system(sys_periodic_orig)
-  dsc = dynamical_correlations(s;Δt = 0.05,nω = 100,ωmax = 1.0)
-  formula_classical_transverse = intensity_formula(dsc,[(:Sx,:Sx),(:Sy,:Sy)]) do k,ω,corr
-    @assert imag(corr[1]) < 1e-8
-    real(corr[1] + corr[2])
-  end
-  params = unit_resolution_binning_parameters(dsc;negative_energies=true)
-  params.binend[1:2] .+= 1
-  params.binend[3] += 3
-  function(;kT_use=0.1)
-    dsc.data .= 0
-    dsc.nsamples[1] = 0
-    s.dipoles .= sys_periodic_orig.dipoles
-    langevin = Langevin(0.05,λ=0.1,kT=kT_use)
-    for j = 1:20
-      for i = 1:1000
-        step!(s,langevin)
-      end
-      add_sample!(dsc,s)
-    end
-
-    formula_classical_transverse_kT = intensity_formula(dsc,[(:Sx,:Sx),(:Sy,:Sy)],kT = kT_use) do k,ω,corr
-      @assert imag(corr[1]) < 1e-8
-      real(corr[1] + corr[2])
-    end
-    is_Sqw_transverse, counts = intensities_binned(dsc,params,formula_classical_transverse)
-    is_Sqw_transverse_kT, counts = intensities_binned(dsc,params,formula_classical_transverse_kT)
-    sum(is_Sqw_transverse ./ counts)/A, sum(is_Sqw_transverse_kT ./ counts)/A
-  end
-end
-dynamic_transverse = mk_dt()
-
-
-  kT_use = 0.1
-  langevin = Langevin(0.05,λ=0.1,kT=kT_use)
-  dsc = dynamical_correlations(sys_periodic;Δt = 0.05,nω = 100,ωmax = 1.0)
-  for j = 1:20
-    for i = 1:1000
-      step!(sys_periodic,langevin)
-    end
-    add_sample!(dsc,sys_periodic)
+# Compute spin wave theory on a finite system size
+function sunny_swt_spectrum(sys;polyatomic = true)
+  all_repeats = allequal([sys.dipoles[i,:] for i = Sunny.eachcell(sys)]) && allequal([sys.coherents[i,:] for i = Sunny.eachcell(sys)])
+  if !all_repeats
+    @warn "Not all unit cells in the system have the same spin data! Collapsing to first unit cell anyway"
   end
 
-  #formula_classical = intensity_formula(isc,:trace)
-  formula_classical_static = intensity_formula(isc,:trace)
-  formula_classical_trace = intensity_formula(dsc,:trace)
-  formula_classical_transverse = intensity_formula(dsc,[(:Sx,:Sx),(:Sy,:Sy)],kT = kT_use) do k,ω,corr
-    @assert imag(corr[1]) < 1e-8
-    real(corr[1] + corr[2])
+  sys_collapsed = Sunny.reshape_supercell_aux(sys, (1,1,1), Sunny.cell_shape(sys))
+  swt = SpinWaveTheory(sys_collapsed;apply_g = false)
+
+  na = Sunny.natoms(sys.crystal)
+  nf = 1 #nf = sys.Ns[1] - 1
+
+  if polyatomic
+    # Find polyatomic required number of unit cells:
+    nbzs = polyatomic_bzs(sys.crystal)
+  else
+    nbzs = [1,1,1]
   end
 
-  params = unit_resolution_binning_parameters(dsc;negative_energies=true)
-  params.binend[1:2] .+= 1
-  params.binend[3] += 3
-  params_instant = copy(params)
-  params_instant = unit_resolution_binning_parameters(isc)
-  params_instant.binend[1:2] .+= 1
-  params_instant.binend[3] += 3
-  bcs = Sunny.axes_bincenters(params)
-
-  is_Sq, counts = intensities_binned(isc,params_instant,formula_classical_static)
-  is_Sqw_transverse, counts = intensities_binned(dsc,params,formula_classical_transverse)
-  is_Sqw_trace, counts = intensities_binned(dsc,params,formula_classical_trace)
-  spin_S = sys.κs[1]
-  nbzs = map(x -> x[end] - x[1],Sunny.axes_binedges(params))[1:3]
-  A = (sys.gs[1][1] * spin_S)^2 * prod(nbzs)
-  println("Classical sum rule: A = $A = (gS)^2 * num BZ")
-  println("            sum(is_Sq) / A = $(sum(is_Sq)/A)")
-  println("sum(is_Sqw_transverse) / A = $(sum(is_Sqw_transverse)/A)")
-  println("     sum(is_Sqw_trace) / A = $(sum(is_Sqw_trace)/A)")
+  comm_ixs = CartesianIndices(ntuple(i -> sys.latsize[i] .* nbzs[i],3))
+  ks_comm = [Sunny.Vec3((i.I .- 1) ./ sys.latsize) for i = comm_ixs]
 
   formula = intensity_formula(swt,:full;kernel = delta_function_kernel)
 
-  # coords = covectors * (q,ω)
-  coords_to_q = inv(params.covectors[1:3,1:3])
-
-  is = Array{Sunny.BandStructure,3}(undef,params.numbins[1:3]...)
-
-  # Loop over qs
-  for ci in CartesianIndices(params.numbins.data[1:3])
-    x_center = bcs[1][ci[1]]
-    y_center = bcs[2][ci[2]]
-    z_center = bcs[3][ci[3]]
-
-    q = SVector{3}(coords_to_q * [x_center;y_center;z_center])
-    is[ci] = formula.calc_intensity(swt,q)
+  Nx,Ny,Nz = sys.latsize .* nbzs
+  S = zeros(ComplexF64,Nx,Ny,Nz,na*nf,3,3) # kx, ky, kz, band, Sμ, Sν
+  disps = zeros(Float64,size(formula.calc_intensity.H,1)÷2,comm_ixs.indices...)
+  for ix = 1:Nx, iy = 1:Ny, iz = 1:Nz
+    band_structure = formula.calc_intensity(swt,ks_comm[ix,iy,iz])
+    for band = 1:length(band_structure.dispersion)
+      disps[band,ix,iy,iz] = band_structure.dispersion[band]
+      S[ix,iy,iz,band,:,:] .= band_structure.intensity[band]
+    end
   end
+  disps, S
+end
 
-  # Spin wave intensities (after summing over bands) are in S²/BZ. To smear this over a bin,
-  # we need [bin size in 1/BZ] * [new value in S²] = [original value in S²/BZ].
-  # The bin size as a fraction of the BZ is:
-  bin_size = prod(params.binwidth[1:3]) / det(params.covectors[1:3,1:3])
+# Automatically choose enough area in reciprocal space
+# to capture all phase-averaging interference phenomena exactly once
+function polyatomic_bzs(crystal)
+  na = Sunny.natoms(crystal)
+  iszero_symprec(x) = abs(x) < crystal.symprec
+  ΔRs = [map(x -> iszero_symprec(x) ? Inf : x,abs.(crystal.positions[i] - crystal.positions[j])) for i = 1:na, j = 1:na]
+  round.(Int64,max.([1,1,1],1 ./ minimum(ΔRs)))
+end
 
-  sw_is_transverse = bin_size * map(x -> sum(map(y -> y[1,1] + y[2,2],x.intensity)),is)
-  sw_is_trace = bin_size * map(x -> sum(map(y -> y[1,1] + y[2,2] + y[3,3],x.intensity)),is)
-  println("Now for spin wave:")
-  println(" sum(sw_is_transverse) / A = $(sum(sw_is_transverse)/A)")
-  println("      sum(sw_is_trace) / A = $(sum(sw_is_trace)/A)")
-#end
+# Establish the common set of binning parameters to use for all histograms
+dsc = dynamical_correlations(sys;Δt = 0.05,nω = 200,ωmax = 10.0,apply_g = false)
+common_params = unit_resolution_binning_parameters(dsc;negative_energies=true)
+common_params.binend[1:3] .+= 1
+common_params.binwidth[4] = 1.0
 
-#finite_spin_wave_theory(sys,(2,3,4))
+# Compute static structure factor
+isc = instant_correlations(sys;apply_g = false)
+add_sample!(isc,sys)
+params_instant = unit_resolution_binning_parameters(isc)
+params_instant.binend[1:3] .+= 1
+is_static, counts = intensities_binned(isc,params_instant,intensity_formula(isc,:trace))
+
+function get_classical_intensities_at_temperature(kT)
+  dsc = dynamical_correlations(sys;Δt = 0.05,nω = 200,ωmax = 10.0,apply_g = false)
+  formula = intensity_formula(dsc,:full;kT = kT)
+
+  langevin = Langevin(0.05,λ=0.3,kT=kT)
+  for j = 1:250
+    for i = 1:1000
+      step!(sys,langevin)
+    end
+    add_sample!(dsc,sys)
+  end
+  is, counts = intensities_binned(dsc,common_params,formula)
+  is ./ counts # Why this division by counts?
+end
+
+# Evalate spin wave theory
+disps_sw, is_bands_sw = sunny_swt_spectrum(sys)
+is_sw = zeros(Float64,common_params.numbins...)
+
+# Bin along energy axis
+for band = 1:size(disps_sw,1)
+  for a = 1:common_params.numbins[1], b = 1:common_params.numbins[2], c = 1:common_params.numbins[3]
+    bin_ix = 1 .+ floor.(Int64,(disps_sw[band,a,b,c] .- common_params.binstart[4]) ./ common_params.binwidth[4])
+    for q = 1:3
+      is_sw[a,b,c,bin_ix] += real(is_bands_sw[a,b,c,band,q,q]) / 2 # Why this factor 2?
+    end
+  end
+end
+
+# Evaluate classical intensities at successively lower temperatures
+temps = 10 .^ range(1,-3,length = 10)
+is_classicals = Array{Float64,4}[]
+for i = 1:length(temps)
+  dat = get_classical_intensities_at_temperature(temps[i])
+  is_classical = zeros(Float64,common_params.numbins...)
+  for a = 1:common_params.numbins[1], b = 1:common_params.numbins[2], c = 1:common_params.numbins[3], d = 1:common_params.numbins[4]
+    for q = 1:3
+      is_classical[a,b,c,d] += real(dat[a,b,c,d][q,q]) 
+    end
+  end
+  push!(is_classicals,is_classical)
+end
+
+# Add 0-magnon term (static structure factor) back into spin wave theory
+# Still missing: Reduced ordered moment correction
+bin_ix_zero = 1 .+ floor.(Int64,(0.0 .- common_params.binstart[4]) ./ common_params.binwidth[4])
+is_sw[:,:,:,bin_ix_zero] .+= is_static
+
+# Compare and plot
+bcs = axes_bincenters(common_params)
+f = Figure()
+ax = Axis(f[1,1],xlabel = "Q",ylabel = "E",title = "Spin Wave")
+hm = heatmap!(ax,1:16,bcs[4],reshape(is_sw,:,common_params.numbins[4]))
+Colorbar(f[1,2],hm)
+ax = Axis(f[2,1],xlabel = "Q",ylabel = "E", title = "Low Temp LL")
+hm = heatmap!(ax,1:16,bcs[4],reshape(is_classicals[end],:,common_params.numbins[4]))
+Colorbar(f[2,2],hm)
+ax = Axis(f[3,1],xlabel = "Q",ylabel = "E", title = "Difference")
+hm = heatmap!(ax,1:16,bcs[4],reshape(is_sw .- is_classicals[end],:,common_params.numbins[4]))
+Colorbar(f[3,2],hm)
+ax = Axis(f[4,1],xlabel = "log10 Temperature", ylabel = "log10 χ² SWT vs LL")
+scatter!(ax,log10.(temps),[log10(norm(is_sw .- is_classicals[i])) for i = 1:length(temps)])
+f
+
