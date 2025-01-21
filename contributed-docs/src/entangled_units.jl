17f717855760068173704d73c221c36586d7d29d
ddahlbom <david.dahlbom@gmail.com>
2025-01-21 16:50:24 -0500

    Draft of entangled units contributed doc

--- contributed-docs/src/entangled_units.jl
+++ contributed-docs/src/entangled_units.jl
@@ -27,27 +27,26 @@
 #
 # The approach to modeling systems with localized entangled is to first build a
 # `System` in the standard way, with an individual spin on each site.
-# Interactions are specified in the standard way. Note that this system must be
+# Interactions are specified as usual in Sunny. Note that this system must be
 # built in `:SUN` mode, even when _S_=1/2. Another important restriction is that
-# any spins which one wishes to entangle must lie within a crystalographic
-# unit cell. This may require reshaping from the conventional unit cell. For the 
-# spin ladder this presents no difficulties. First specify a the crystal.
+# any spins which one wishes to entangle must lie within a crystalographic unit
+# cell. This may require reshaping from the conventional unit cell or displacing
+# the atom positions within the unit cell. We plan to automate this step in
+# later versions. For the spin ladder, there is no need to worry about this
+# step. 
+
+# Specify a the crystal.
 
 using Sunny, LinearAlgebra, GLMakie
 
-latvecs = [
-    1 0 0
-    0 1 0
-    0 0 2
-]
-positions = [[0, 0, 0], [0, 1/2 + 0.001, 0]]
+latvecs = lattice_vectors(1, 1, 2, 90, 90, 90)
+positions = [[0, 0, 0], [0, 1/2 - 1e-4, 0]]
 crystal = Crystal(latvecs, positions)
 view_crystal(crystal)
 
-# We are interested in a quasi-1D system, without periodic boundary conditions
-# along the b axis. For this reason, we set the second site slightly off of 
-# the `[0, 0.5, 0]` position, which would induce Sunny to identify the bonds
-# between the two sides (1 -> 2 and 2 -> 1) as equivalent.
+# To model a quasi-1D system, symmetry must be broken along the b-axis by
+# making bonds 1 -> 2 and 2 -> 1 inequivalent. This is achieved by offsetting
+# the position of the second atom slightly from `[0, 0.5, 0]`.
 
 # Specify a system and the two exchange interactions, J (rungs) and J′
 # (lengthwise bonds). 
@@ -55,19 +54,20 @@ view_crystal(crystal)
 J = 1
 J′ = 0.2J
 sys = System(crystal, [1 => Moment(s=1/2, g=2)], :SUN; dims=(2, 1, 1))
-set_exchange!(sys, J′, Bond(1, 1, [1, 0, 0]))
 set_exchange!(sys, J, Bond(1, 2, [0, 0, 0]))
+set_exchange!(sys, J′, Bond(1, 1, [1, 0, 0]))
 
-# Examine the behavior of this system when we randomize the spins and minimize
-# the energy.
+# This completes specification of the model without entanglement between the
+# sites bonded by rungs. Examine now a ground state of this system.
 
 randomize_spins!(sys)
 minimize_energy!(sys)
 plot_spins(sys)
 
+
 # ## Spin Wave Calculations
 
-# The result is the expected classical ordering, with full-length dipoles
+# The last result is the expected classical ordering, with full-length dipoles
 # arranged antiferromagnetically. This results in a `q=(π,π)` ordering. Because
 # this model is Heisenberg, the Hamiltonian has an SU(2) symmetry, and any such
 # ground state breaks this symmetry. This will lead to a Goldstone mode, as can
@@ -80,11 +80,11 @@ res = intensities(swt, qs; energies, kernel=gaussian(; fwhm=0.2))
 plot_intensities(res)
 
 # This result is incorrect for such a small J′. Instead, the ground state on
-# each bond should be a singlet, i.e. a non-magnetic ground state.
-# Correspondingly, the excitations should be gapped singlet-triplet excitations.
-# This can be reproduced using the entangled units formalism. An
-# `EntangledSystem` is constructed from an ordinary `System` by providing a list
-# of sites "to entangle" within each unit cell.
+# each bond should be a singlet, i.e. non-magnetic. Correspondingly, the
+# excitations should be gapped singlet-triplet excitations. This can be
+# reproduced using the entangled units formalism. An `EntangledSystem` is
+# constructed from an ordinary `System` by providing a list of sites "to
+# entangle" within each unit cell.
 
 esys = Sunny.EntangledSystem(sys, [(1, 2)])
 randomize_spins!(esys)
@@ -96,13 +96,13 @@ plot_spins(esys)
 # vector is now `q=0`, so the system should be reshaped into the magnetic unit
 # cell (one bond) before performing spin wave calculations.
 
-esys = reshape_supercell(esys, I)
+esys = reshape_supercell(esys, I(3))
 plot_spins(esys)
 
 # Next create a `SpinWaveTheory` and calculate intensities just as would be
 # done for an ordinary Sunny `System`.
 
-eswt = SpinWaveTheory(esys; measure=ssf_trace(esys)) ## TODO: ssf functions for esys
+eswt = SpinWaveTheory(esys; measure=ssf_trace(esys))
 res = intensities(eswt, qs; energies, kernel=gaussian(; fwhm=0.2))
 plot_intensities(res)
 
@@ -119,7 +119,7 @@ plot_intensities(res)
 # along the a-axis so that we have sufficient momentum resolution to resolve the
 # dispersion. 
 
-esys = repeat_periodically(esys, (10, 1, 1))
+esys = repeat_periodically(esys, (20, 1, 1))
 
 damping = 0.2
 kT = 0.1J
@@ -136,7 +136,7 @@ end
 
 sc = SampledCorrelations(esys; energies, dt, measure=ssf_trace(esys))
 
-for _ in 1:10
+for _ in 1:100
     for _ in 1:300
         step!(esys, integrator)
     end
@@ -170,7 +170,7 @@ for _ in 1:500
     step!(esys, integrator)
 end
 
-for _ in 1:10
+for _ in 1:100
     for _ in 1:300
         step!(esys, integrator)
     end
@@ -188,3 +188,74 @@ fig
 # A pseudogap persists even well above the ordering temperature, in agreement
 # with exact solutions. This is in contrast with traditional Landau-Lifshitz
 # dynamics, which remains gapless throughout all temperatures.
+
+## Material Example: Ba₃Mn₂O₈
+
+# As a more realistic example, this section briefly demonstrates how to reproduce
+# the zero-field dispersion of Ba₃Mn₂O₈. This material consists of triangular lattice
+# of $S=1$ bilayers in an ABC stacking.
+
+# First load the crystal and extract the magnetic subcrystal.
+
+crystal_full = Crystal(joinpath(@__DIR__, "Ba3Mn2O8.cif"); symprec=0.01)
+crystal = subcrystal(crystal_full, "Mn1")
+view_crystal(crystal)
+
+# Each site hosts a single $S=1$ spin subject to an easy-axis anisotropy. The
+# spins interact with each other antiferromagnetically through a number of
+# Heisenberg exchange interactions.
+
+sys = System(crystal, [1 => Moment(s=1, g=2)], :SUN)
+
+J0 = 1.642
+J1 = 0.118
+J2 = 0.256
+J3 = 0.142
+J4 = 0.037
+set_exchange!(sys, J0, Bond(1, 2, [0, 0, 0]))
+set_exchange!(sys, J1, Bond(2, 3, [0, 0, 0]))
+set_exchange!(sys, J2, Bond(1, 1, [1, 0, 0]))
+set_exchange!(sys, J3, Bond(1, 2, [1, 0, 0]))
+set_exchange!(sys, J4, Bond(4, 5, [0, 1, 0]))
+
+D = -0.032
+set_onsite_coupling!(sys, S -> D*S[3]^2, 1)
+
+# The strong antiferromagnetic exchange on the `1 -> 2`, `3 -> 4` and `5 -> 6`
+# bonds results in dimerization and a ground state that is a product of
+# singlets. This physics can be capture by treating each dimer as a single
+# entangled unit.
+
+dimers = [(1, 2), (3, 4), (5, 6)]
+esys = Sunny.EntangledSystem(sys, dimers)
+
+# Examine the ground state.
+
+randomize_spins!(esys)
+minimize_energy!(esys)
+plot_spins(esys)
+
+# Observe that there is effectively no magnetic moment, up to numerical
+# roundoff. This ground state can then be used to perform a spin wave
+# calculation.
+
+formfactors = [1 => FormFactor("Mn5")]
+measure = ssf_perp(esys; formfactors)
+swt = SpinWaveTheory(esys; measure)
+
+fwhm = 0.295
+points = [
+    [0.175, 0.175, 1.5],
+    [0.85, 0.85, 1.5],
+    [0.85, 0.85, 3],
+    [0.0, 0.0, 3],
+    [0.0, 0.0, 8],
+]
+qpts = q_space_path(crystal, points, 400)
+energies = range(0.0, 4.0, 400)
+res = intensities(swt, qpts; energies, kernel=gaussian(; fwhm))
+
+plot_intensities(res)
+
+# Compare the resulting dispersion with the experimental data reported in
+# [Stone et al., PRL 100 (2008)](https://arxiv.org/abs/0804.4437).
